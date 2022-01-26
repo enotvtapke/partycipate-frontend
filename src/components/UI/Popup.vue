34e8f556040c516896efde6a4d77e8270af0e24e
enotvtapke <super-stupnikow@mail.ru>
2022-01-26 13:51:11 +0400

    Add invite form to event

---
+++ src/components/UI/Popup.vue
@@ -0,0 +1,37 @@
+<template>
+    <div @click="TogglePopup()" class="popup">
+        <div @click.stop class="popup-inner">
+            <button class="popup-close" @click="TogglePopup()">
+                X
+            </button>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Popup',
+    props: ['TogglePopup']
+}
+</script>
+
+<style lang="scss" scoped>
+.popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.2);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .popup-inner {
+        background: #FFF;
+        padding: 1rem;
+    }
+}
+</style>
