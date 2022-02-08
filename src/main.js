97c05d8bc00e3d5a7050d545cd2739402f5772dd
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-02-09 00:12:27 +0300

    Changed base url and added CORs access control header for production

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
+window.axios = require('axios')
+window.axios.defaults.baseURL = 'https://partycipate-api.herokuapp.com'
+window.axios.defaults.headers = {
+    'Access-Control-Allow-Origin': 'https://partycipate.herokuapp.com'
+}
+
+// window.axios = require('axios')
+// window.axios.defaults.baseURL = 'http://localhost:8080'
+// window.axios.defaults.headers = {
+//     'Access-Control-Allow-Origin': 'http://localhost:3000'
+// }
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
